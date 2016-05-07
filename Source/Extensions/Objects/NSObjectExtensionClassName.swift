7a6c2b4c85f2a3ecabff34537822548464e13155
Igor Matyushkin <igormatyushkin014@gmail.com>
2016-05-07 14:38:14 +0300

    Added framework's files

---
+++ Source/Extensions/Objects/NSObjectExtensionClassName.swift
@@ -0,0 +1,37 @@
+//
+//  NSObjectExtensionClassName.swift
+//  Sirius
+//
+//  Created by Igor Matyushkin on 07.05.16.
+//  Copyright © 2016 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public extension NSObject {
+    
+    // MARK: Public static methods
+    
+    public static func className(includeNamespace includeNamespace: Bool) -> String {
+        let classNameWithNamespace = NSStringFromClass(self)
+        
+        if includeNamespace {
+            return classNameWithNamespace
+        } else {
+            let segments = classNameWithNamespace.componentsSeparatedByString(".")
+            let classNameWithoutNamespace = segments.last ?? ""
+            return classNameWithoutNamespace
+        }
+    }
+    
+    
+    // MARK: Private static methods
+    
+    
+    // MARK: Public object methods
+    
+    
+    // MARK: Private object methods
+    
+}
+
